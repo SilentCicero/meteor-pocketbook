e678666b569a7726e78d499eaf3a602159ba125a
SilentCicero <silentcicero@outlook.com>
2015-10-10 23:21:23 -0400

    Minor Updates

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'silentcicero:meteor-pocketbook',
   summary: 'A Mini-Wallet for Meteor dApps',
-  version: '0.0.5',
+  version: '0.0.6',
   git: 'http://github.com/SilentCicero/meteor-pocketbook'
 });
 
